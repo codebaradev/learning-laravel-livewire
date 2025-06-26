3908bfc6b3da15d96d729c29a8e1031910771a56
codebaradev <bahnasep@gmail.com>
2025-06-27 06:43:22 +0800

    feat: Simple Counter App

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">Increment</button>
+    <button wire:click="decrement">Decrement</button>
+    <button wire:click="resetCount">Reset</button>
+</div>
